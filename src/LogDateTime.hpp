fa83f55fde3ab761921b2af0b49f9f61aed4afab
hypson <duol0601@mail.ru>
2019-12-12 10:08:56 +0500

    change ctr

--- src/LogDateTime.hpp
+++ src/LogDateTime.hpp
@@ -10,7 +10,7 @@ struct LogDateTime {
     uint8_t minute;
     uint8_t period;
     LogDateTime() : year(0), month(0), day(0), hour(0), minute(0), period(0) {}
-    LogDateTime(uint8_t tperiod) : year(0), month(0), day(0), hour(0), minute(0), period(tperiod) {}
+    LogDateTime(uint8_t tperiod) : LogDateTime() { period = tperiod; }
     LogDateTime(tm& t, uint8_t tperiod) : 
         year(static_cast<uint16_t>(t.tm_year + 1900)),
         month(static_cast<uint8_t>(t.tm_mon + 1)),
